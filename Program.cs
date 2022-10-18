8589cdb870582df93b1d3bf602341a519c32c67e
wahek <wahek1999@mail.ru>
2022-10-18 23:41:00 +0300

    Вторая задача

--- Program.cs
+++ Program.cs
@@ -3,9 +3,9 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-Console.WriteLine("Введите число N: ");
-int N = int.Parse(Console.ReadLine()!);
-PrintNumbers(N);
+// Console.WriteLine("Введите число N: ");
+// int N = int.Parse(Console.ReadLine()!);
+// PrintNumbers(N);
 
 void PrintNumbers(int n)
 {
@@ -16,4 +16,27 @@ void PrintNumbers(int n)
         PrintNumbers(n - 1);
     }
 
+}
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int M = int.Parse(Console.ReadLine()!);
+if(N > M){
+    int temp = N;
+    N = M;
+    M = temp;
+}
+if(M == N) Console.WriteLine(N);
+else{Console.WriteLine(GetSumFromMinToMax(N, M));
+}
+
+int GetSumFromMinToMax(int n, int m){
+    if(n == m) return(m);
+    else return n + GetSumFromMinToMax(n + 1, m);
 }
